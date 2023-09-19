8c8074c9593249b39b261b65f6201c37fed7a8b1
CrioUser <crio-user@criodo.com>
2023-09-20 01:09:17 +0530

    “ME_QTRIPDYNAMIC_MODULE_CITIES”

--- frontend/modules/landing_page.js
+++ frontend/modules/landing_page.js
@@ -16,14 +16,30 @@ async function init() {
 async function fetchCities() {
   // TODO: MODULE_CITIES
   // 1. Fetch cities using the Backend API and return the data
-
+  try {
+    const res = await fetch(config.backendEndpoint+"/cities");
+    return await res.json();
+  } catch (error) {
+    return null;
+  }
 }
 
 //Implementation of DOM manipulation to add cities
 function addCityToDOM(id, city, description, image) {
   // TODO: MODULE_CITIES
   // 1. Populate the City details and insert those details into the DOM
+  const tile = document.createElement("div");
+  tile.className = "col col-12 col-sm-6 col-lg-3";
+  tile.innerHTML = 
+  `<a href="pages/adventures/?city=${id}" id="${id}" class="tile">
+    <img src="${image}" alt="${city}" />
+      <div class="tile-text bottom-0 start-50 translate-middle text-center">
+        <h6 class="fs-5">${city}</h6>
+        <p class="fs-6 mb-0">100+ places</p>
+      </div>
+  </a>`;
 
+  document.getElementById("data").append(tile);
 }
 
 export { init, fetchCities, addCityToDOM };
